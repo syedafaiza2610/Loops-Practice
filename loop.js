0b13653a710c63a90ce6fc434c032222fae04f37
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-18 14:06:00 +0500

    first commit

---
+++ loop.js
@@ -0,0 +1,22 @@
+// var i ;
+// for(let i=1; i<=100; i=i+10){
+//     for(j=i; j < i+10; j++){
+//         document.write(j+" ");
+//     }
+//     document.write("<br>")
+// }
+// for (let index = 1; index <= 20; index+=2) {
+//    document.write(index + " ")
+    
+// }
+
+// var arr = ['Saira','Faiza','Shahida'];
+// var arr2 =['Naqi','Parveen'];
+// for (var i=0; i<arr.length; i++) {
+
+//    for (var j=0; j<arr2.length; j++) {
+//     document.write(arr[i] + " " + arr2[j],"<br>");
+//     }
+
+    
+// }
