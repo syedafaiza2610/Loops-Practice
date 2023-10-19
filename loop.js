b51fca53a3465bb0d37088e66326e912a448ca1e
syeda Faiza <syedafaiza2610@gmail.com>
2023-10-19 10:04:26 +0500

    array

--- loop.js
+++ loop.js
@@ -20,3 +20,79 @@
 
     
 // }
+
+
+// 1. Declare an empty array using JS literal notation to store 
+// student names in future. 
+
+// var students = [];
+// students.push("");
+// students.push("");
+// students.push("");
+// students.push("");
+// document.write(students);
+
+
+// 2. Declare an empty array using JS object notation to store 
+// student names in future. 
+
+// var name1 ={};
+// 3. Declare and initialize a strings array.
+// var str =["Faiza" ,"Sami","Saira","Shahida"] ;
+// document.write(typeof("faiza"));
+
+// 4. Declare and initialize a numbers array. 
+// var str =[1,2,3,4,5,6,7] ;
+// document.write(typeof(2));
+// 5. Declare and initialize a boolean array.
+// var bool =[true,false]
+// document.write(typeof(true));
+
+
+// 8.  Declare and Initialize an array and store available education 
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, 
+// M. Phil., PhD). Show the listed qualifications in your browser 
+// like: 
+
+// var qualification =["SSC","HSC","BCS","BS","BCOM","MS","M.PHILL","PHD"]
+// for(i=1; i<qualification.length; i++){
+//     document.write(i + ")" + qualification[i] ,"<br>")
+// }
+
+// Write a program to store 3 student names in an array. Take 
+// another array to store score of these three students. Assume 
+// that total marks are 500 for each student, display the scores & 
+// percentages of students like:
+
+//  var studentNames = ["John", "Alice", "Bob"];
+// for (var i = 0; i < studentNames.length; i++) {
+//   var studentName = studentNames[i];
+//   var studentScore = studentScore[i];
+//   var percentage = (studentScore / 500) * 100;
+
+//   document.write("Student: " + studentName);
+//   document.write("Score: " + studentScore);
+//   document.write("Percentage: " + percentage.toFixed(2) + "%");
+//   document.write();
+// }
+
+// var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// // var arr2 = arr.join(" ");
+// for(let i = 0; i<arr.length; i++){
+// document.write(arr[i] + "<br>");
+// }
+
+
+// var arr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// // var arr2 = arr.join(" ");
+// for(let i = 0; i < arr.length; i++) {
+//   for(let j=0; j< arr[i]; j++);{
+// document.write(arr[i][j] + "<br>");
+//   }
+// }
+
+// array.forEach(element => {
+  
+// });
+
+
